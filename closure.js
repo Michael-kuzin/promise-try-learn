6fe77f207bb9da8cfb6efa7f09eefbb626b740ef
Vadym Kuzin/LGEUS Advanced Platform(vadym.kuzin@lge.com) <vadym.kuzin@lgsvl.net>
2020-06-23 09:01:03 -0700

    chnge closure

--- closure.js
+++ closure.js
@@ -140,20 +140,93 @@
 
 
 
-let obj = {
-  number: 0,
-  click: (function() {
-    let c = this.number;
-    let a = 0;
-    let b = 1;
-    return function() {
-      c = a;
-      a = b;
-    }
-  })(),
-}
 
-obj.click();
-obj.click();
-obj.click();
-console.log(obj.number)
+// let obj = {
+//   number:2,
+//   func:(function () {
+//         let a = 0;
+//         let b = 1;
+//         return function(n) {
+//           for(let i = 0;i < n; i++) {
+//             console.log(i);
+//             let c = a + b;
+//             a = b;
+//             b = c;
+//             this.number = b;
+//           }
+//         }
+//   }())
+// }
+// obj.func(5);
+// console.log(obj.number);
+// // console.log(obj.func());
+// console.log(obj.func());
+
+
+
+
+// if (!("a" in global)) {
+//     var a = 1;
+// }
+// console.log(a);
+
+
+
+// var b = function a(x) {
+//     x && a(--x);
+// };
+// console.log(a);
+
+
+
+var users = [{
+  name: "Вася",
+  surname: 'Иванов',
+  age: 20
+}, {
+  name: "Петя",
+  surname: 'Чапаев',
+  age: 25
+}, {
+  name: "Маша",
+  surname: 'Медведева',
+  age: 18
+}];
+
+function byField(field) {
+    return function(a, b) {
+      return a[field] > b[field] ? 1 : -1;
+    }
+  }
+
+users.sort(byField('лгегьлеыл'));
+users.forEach(function(user) {
+  console.log(user.name)
+});
+
+users.sort(byField('age'));
+users.forEach(function(user) {
+  console.log(user.name)
+});
+
+
+
+
+// function inBetween(a, b) {
+//   return function(x) {
+//     return x >= a && x <= b;
+//   };
+// }
+//
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log( arr.filter(inBetween(3, 6)) );
+//
+//
+// function inArray(arr) {
+//   return function(x) {
+//     return arr.includes(x);
+//   };
+// }
+//
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log( arr.filter(inArray([1, 2, 10])) );
